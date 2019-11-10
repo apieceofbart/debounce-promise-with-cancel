5ed879f85c0b67f9a02bdb2e31da5adb92564d59
Bartek Kozera <bartek.kozera@gmail.com>
2019-11-10 13:43:11 +0100

    add source code and typescript

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+function debounce<T, A>(fn: (...args: A[]) => T, ms = 0) {
+  let timer: ReturnType<typeof setTimeout> | undefined = undefined
+  let cancelled = false
+
+  const debounced = (...args: A[]) => {
+    cancelled = false
+    if (timer) {
+      clearTimeout(timer)
+    }
+    return new Promise<T>(resolve => {
+      timer = setTimeout(() => {
+        if (!cancelled) {
+          resolve(fn(...args))
+        }
+      }, ms)
+    })
+  }
+
+  debounced.cancel = function() {
+    cancelled = true
+  }
+
+  return debounced
+}
+
+export default debounce
+export { debounce }
